fc1bb79805382980358409e58de27ac3fbda257c
ewilkinson896 <erin.wilkinson@Kainos.com>
2025-10-23 14:33:34 +0100

    added missing migration and fixed issues

--- drizzle/0004_broad_blindfold.sql
+++ drizzle/0004_broad_blindfold.sql
@@ -3,18 +3,14 @@ CREATE TABLE `__new_applications` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`user_id` integer NOT NULL,
 	`job_role_id` integer NOT NULL,
-	`cv_file_name` text NOT NULL,
-	`cv_file_path` text NOT NULL,
-	`cv_file_type` text NOT NULL,
-	`cv_file_size` integer NOT NULL,
+	`cv_text` text NOT NULL,
 	`status` text DEFAULT 'in progress' NOT NULL,
 	`created_at` text NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`job_role_id`) REFERENCES `job_roles`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-INSERT INTO `__new_applications`("id", "user_id", "job_role_id", "status", "created_at", "cv_file_name", "cv_file_path", "cv_file_type", "cv_file_size") 
-SELECT "id", "user_id", "job_role_id", "status", "created_at", 'legacy-cv.txt', '', 'text/plain', 0 FROM `applications`;--> statement-breakpoint
+INSERT INTO `__new_applications`("id", "user_id", "job_role_id", "cv_text", "status", "created_at") SELECT "id", "user_id", "job_role_id", "cv_text", "status", "created_at" FROM `applications`;--> statement-breakpoint
 DROP TABLE `applications`;--> statement-breakpoint
 ALTER TABLE `__new_applications` RENAME TO `applications`;--> statement-breakpoint
 PRAGMA foreign_keys=ON;
