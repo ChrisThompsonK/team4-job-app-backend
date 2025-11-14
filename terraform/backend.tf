c18ad2eb2742060ed69a29586ec69ef438eda806
alexjones06B <alex.jones@kainos.com>
2025-11-14 15:01:54 +0000

    chore: Update Terraform backend to use team4-backend resource group

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "team4jobappterraformstate"
+    resource_group_name  = "team4-backend"
+    storage_account_name = "team4tfstate"
     container_name       = "tfstate"
     key                  = "team4-job-app-backend.tfstate"
   }
